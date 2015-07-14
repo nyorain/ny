0d1152e2381ed2da3f726e23d437b9a2aa352fa8
nyorain <nyorain@gmail.com>
2015-07-14 16:49:45 +0200

    started winapi branch

--- include/ny/winapi/winapiAppContext.hpp
+++ include/ny/winapi/winapiAppContext.hpp
@@ -34,6 +34,19 @@ public:
     ~winapiAppContext();
 
     virtual bool mainLoop();
+
+    virtual void startDataOffer(dataSource& source, const image& img, const window& w, const event* ev){}
+    virtual bool isOffering() const { return 0; }
+    virtual void endDataOffer(){}
+
+    virtual dataOffer* getClipboard(){ return nullptr; }
+    virtual void setClipboard(dataSource& source, const event* ev){}
+
+    //data specifications
+    void setClipboard(const std::string& str){}
+    void setClipboard(const image& str){}
+
+
     virtual LRESULT eventProc(HWND, UINT, WPARAM, LPARAM);
 
     void registerContext(HWND w, winapiWindowContext* c);
