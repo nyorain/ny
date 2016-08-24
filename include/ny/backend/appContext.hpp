d7d2988790715ec101d312eb6c4fc0822b45fe05
nyorain <nyorain@gmail.com>
2016-08-24 20:32:09 +0200

    threading model rework #1 - concepts

--- include/ny/backend/appContext.hpp
+++ include/ny/backend/appContext.hpp
@@ -49,36 +49,36 @@ public:
 	///\sa KeyboardContext
 	virtual KeyboardContext* keyboardContext() = 0;
 
-	///Dispatches all retrieved events to the registered handlers using the given EventDispatcher.
+	///Dispatches all retrieved events to the the given EventDispatcher.
 	///Does only dispatch all currently queued events and does not wait/block for new events.
-	///This function is not threadsafe, i.e. it is undefined what happens it other threads
-	///use the given EventDispatcher until the function returns.
+	///Shall only be called from the ui thread.
 	///\return false if the display conncetion was destroyed (or an error occurred),
 	///true otherwise (if all queued events were dispatched).
-	virtual bool dispatchEvents(EventDispatcher&) = 0;
+	virtual bool dispatchEvents(EventDispatcher& control) = 0;
 
 	///Blocks and dispatches all incoming display events until the stop function of the loop control
 	///is called or the display conncection is closed by the server (e.g. an error or exit event).
+	///Shall only be called from the ui thread.
 	///\param control A AppContext::LoopControl object that can be used to stop the loop from
 	///inside or from another thread.
 	///\return true if loop was exited because stop() was called by the LoopControl.
-	///\return false if loop was exited because the display conncetion was destroyed.
-	virtual bool dispatchLoop(EventDispatcher&, LoopControl&) = 0;
+	///\return false if loop was exited because the display conncetion was destroyed or an error
+	///occured.
+	virtual bool dispatchLoop(LoopControl& control) = 0;
 
-	///Blocks ans dispatches all incoming events from the display and queued events inside
-	///the EventDispatcher until the stop function of the given loop control is called or
-	///the display connection is closed by the server.
-	///This function only accepts an ThreadedEventDispatcher as dispatcher paramter and
-	///guarantess furthermore threadsafety, i.e. events from other threads dispatched over the
-	///given EventDispatcher will be sent.
+	///Blocks and dispatches all incoming events from the display and queued events inside
+	///the EventDispatcher and AppContext until the stop function of the given loop control is 
+	///called or the display connection is closed by the server.
 	///This function itself will take care of dispatching all events, so the dispatcher
 	///loop of the given dispatcher should not be run in different threads.
-	///\sa ThreadedEventDispatcher
+	///Shall only be called from the ui thread.
 	///\param control A AppContext::LoopControl object that can be used to stop the loop from
 	///inside or from another thread.
 	///\return true if loop was exited because stop() was called by the LoopControl.
-	///\return false if loop was exited because the display conncetion was destroyed.
-	virtual bool threadedDispatchLoop(ThreadedEventDispatcher&, LoopControl&) = 0;
+	///\return false if loop was exited because the display conncetion was destroyed or an error
+	///occured.
+	///\sa ThreadedEventDispatcher
+	virtual bool threadedDispatchLoop(EventDispatcher&, LoopControl&) = 0;
 
 
 	///Sets the clipboard to the data provided by the given DataSource implementation.
